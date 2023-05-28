ebe44dcda31b35f39003c98f1214acf78929baf7
Hyunfor <navel5148@gmail.com>
2023-05-28 17:17:14 +0900

    BoardDAO 게시글 작성 및 조회수 코딩 끝

--- src/com/mis/dao/BoardDAO.java
+++ src/com/mis/dao/BoardDAO.java
@@ -7,30 +7,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.mis.dto.BoardVO;
-import com.mis.dto.ProductVO;
 
 import util.DBManager;
 
 public class BoardDAO {
-	
-	private BoardDAO(){	
+
+	private BoardDAO() {
 	}
-	
+
 	private static BoardDAO instance = new BoardDAO();
-	
-	public static BoardDAO getInstance(){
+
+	public static BoardDAO getInstance() {
 		return instance;
 	}
-	
+
 	// 게시글 목록 가져오기
-	public List<BoardVO> selectAllBoards(){
+	public List<BoardVO> selectAllBoards() {
 		String sql = "SELECT * FROM BOARD ORDER BY NUM DESC";
-		
+
 		List<BoardVO> list = new ArrayList<BoardVO>();
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		
+
 		try {
 
 			conn = DBManager.getConnection();
@@ -39,9 +38,9 @@ public class BoardDAO {
 
 			// 결과 값으로 리스트에 담기
 			while (rs.next()) {
-				
+
 				BoardVO bVo = new BoardVO();
-				
+
 				bVo.setNum(rs.getInt("num"));
 				bVo.setName(rs.getString("name"));
 				bVo.setEmail(rs.getString("email"));
@@ -69,41 +68,68 @@ public class BoardDAO {
 		return list;
 
 	}
-	
+
 	// 게시글 작성
-	public void insertBoard(BoardVO bVo){
-		String sql = "SELECT * FROM BOARD ORDER BY NUM DESC";
-		
-		List<BoardVO> list = new ArrayList<BoardVO>();
+	public void insertBoard(BoardVO bVo) {
+		String sql = "INSERT INTO BOARD(" + "num, name, email, pass, title, content)"
+				+ "values(board_seq.nextval, ?, ?, ?, ?, ?)";
+
 		Connection conn = null;
 		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		
+
 		try {
 
 			conn = DBManager.getConnection();
 			pstmt = conn.prepareStatement(sql);
-			rs = pstmt.executeQuery();
 
-			// 결과 값으로 리스트에 담기
-			while (rs.next()) {
-				
+			pstmt.setString(1, bVo.getName());
+			pstmt.setString(2, bVo.getEmail());
+			pstmt.setString(3, bVo.getPass());
+			pstmt.setString(4, bVo.getTitle());
+			pstmt.setString(5, bVo.getContent());
 			
-
-			}
+			pstmt.executeUpdate();
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			try {
-				DBManager.close(conn, pstmt, rs);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+				try {
+					DBManager.close(conn, pstmt);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 		}
-
-		return list;
-
+			
 	}
 	
+	// 게시글 조회수 증가
+	public void updateReadCount(String num){
+		String sql = "UPDATE BOARD SET READCOUNT=READCOUNT+1 WHERE NUM=?";
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		try {
+
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setString(1, num);
+			
+			pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+				try {
+					DBManager.close(conn, pstmt);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		}
+		
+	}
+
 }
